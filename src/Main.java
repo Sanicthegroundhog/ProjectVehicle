6b77d242608dbf389e30c0cd05c8506d05ae13b6
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-09-16 12:44:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Vehicle myCar = new Vehicle("Ford", "Fusion");
+        System.out.println(myCar.getMake());
+        myCar.setMake("Chevy");
+        System.out.println(myCar.getMake());
+
+    }
+}
